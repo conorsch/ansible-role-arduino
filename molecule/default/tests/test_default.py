fcb71d1789a92c9e04e34d3e99890e274dfb9391
Conor Schaefer <conor.schaefer@gmail.com>
2017-10-09 16:51:01 -0700

    Creates Arduino IDE role
    Installs the latest IDE version from the website (version is hardcoded
    in role vars; can be overridden), extracts to /opt, and runs the
    installer script with normal user privileges.
    
    Adds helper scripts (arduino-push, arduino-verify)
    These are extremely common tasks and the IDE CLI is clunky, so drop in
    some wrapper scripts to call these tasks easily.

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,31 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+ARDUINO_VERSION = "1.8.5"
+
+def test_arduino_download_directory(host):
+    f = host.file('/opt/arduino')
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_arduino_ide_tarball(host):
+    f = host.file("/opt/arduino/arduino-{}-linux64.tar.xz".format(ARDUINO_VERSION))
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_arduino_ide_directory(host):
+    f = host.file("/opt/arduino/arduino-{}".format(ARDUINO_VERSION))
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'root'
+    assert f.group == 'root'
